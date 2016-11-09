0e95bcfcb5d0239c757c06ed5affcadad27a6992
andreas-optimizely <andreas@optimizely.com>
2016-11-09 12:12:48 -0800

    update url permissions in manifest

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
+'use strict';
 const gulp = require("gulp"),
       babel = require("gulp-babel"),
       concat = require("gulp-concat"),
       rename = require('gulp-rename'),
       uglify = require('gulp-uglify'),
-      pug = require('gulp-pug');
+      pug = require('gulp-pug'),
+      zip = require('gulp-zip');
 
 const DEST = 'build',
       IMAGE_DEST = 'build/images';
 
-gulp.task("default", ['js','views','manifest', 'images']);
+gulp.task("default", ['js','manifest','images']);
 
 gulp.task('js', ()=>{
   return gulp.src("src/js/index.js")
@@ -35,6 +37,14 @@ gulp.task('manifest', ()=>{
 gulp.task('images', ()=>{
   return gulp.src('src/images/*')
     .pipe(gulp.dest(IMAGE_DEST));
+});
+
+gulp.task('compile:prod', ['default'], () => {
+  let date = new Date();
+  let dateString = (date.getMonth()+1) + '-' + date.getDate() + '-' + date.getFullYear();
+  return gulp.src('build/*')
+    .pipe(zip(dateString + '-ua-spoofer-build.zip'))
+    .pipe(gulp.dest('prod-build'));
 });
 
 gulp.task('watch', function(){
